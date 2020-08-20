e3297babe1b9ad6d8983dc2af805a6545279457f
Aditya Pandey - a0p02ll <Aditya.Pandey@walmartlabs.com>
2020-08-20 09:52:17 +0530

    Added tutorial files

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://admin:admin@cluster0-shard-00-00-oosu8.mongodb.net:27017,cluster0-shard-00-01-oosu8.mongodb.net:27017,cluster0-shard-00-02-oosu8.mongodb.net:27017/devconnector?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
